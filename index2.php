6ab05162083d1cbbc98e5517849ad9b3cf503781
Kauann_guimaraes3 <kauannguimaraes3@gmail.com>
2022-06-07 17:45:02 -0300

    localhostprep

--- index2.php
+++ index2.php
@@ -1,6 +1,5 @@
 <?php 
-    //$api = new PinPointDAO;
-    //header('Content-Type: application/json');
-    //echo $api->searchPinpoints();
-    echo "ola";
+    $api = new PinPointDAO;
+    header('Content-Type: application/json');
+    echo $api->searchPinpoints();
 ?>
