a6109d134ac02a32b90576d3672c03465a9c860b
Kauann_guimaraes3 <kauannguimaraes3@gmail.com>
2022-06-07 16:55:10 -0300

    index2 alter

--- index2.php
+++ index2.php
@@ -1,5 +1,5 @@
 <?php 
-    $api = new PinPointDAO;
-    header('Content-Type: application/json');
-    echo $api->searchPinpoints();
+    //$api = new PinPointDAO;
+    //header('Content-Type: application/json');
+    //echo $api->searchPinpoints();
     echo "ola"
